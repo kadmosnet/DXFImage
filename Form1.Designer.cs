b373ab326a02031b541092593b6852bb59dd2c53
Daniele Alberti <kadmos@kadmos.com>
2023-07-12 11:05:40 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace DXFImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DXFReaderNET.DxfDocument dxfDocument1 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor1 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables1 = new DXFReaderNET.Objects.RasterVariables();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dxfReaderNETControl1
+            // 
+            this.dxfReaderNETControl1.AutoSize = true;
+            this.dxfReaderNETControl1.ContinuousHighlight = false;
+            this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
+            dxfDocument1.ActiveLayout = "Model";
+            dxfDocument1.Comments = null;
+            aciColor1.Index = ((short)(256));
+            aciColor1.IsByBlock = false;
+            aciColor1.IsByLayer = true;
+            aciColor1.UseTrueColor = false;
+            dxfDocument1.CurrentColor = aciColor1;
+            dxfDocument1.CurrentElevation = 0D;
+            dxfDocument1.CurrentLayer = "0";
+            dxfDocument1.CurrentLineTypeName = "ByLayer";
+            dxfDocument1.CurrentLineTypeScale = 1D;
+            dxfDocument1.CurrentTextSize = 2.5D;
+            dxfDocument1.CurrentTextStyle = "STANDARD";
+            dxfDocument1.CurrentThickness = 0D;
+            dxfDocument1.DateCreated = new System.DateTime(2023, 7, 12, 9, 8, 0, 278);
+            dxfDocument1.DateLastEdited = new System.DateTime(2023, 7, 12, 9, 8, 0, 278);
+            dxfDocument1.Handle = "0";
+            dxfDocument1.IsBinary = false;
+            dxfDocument1.Modified = false;
+            dxfDocument1.Name = null;
+            dxfDocument1.Owner = null;
+            rasterVariables1.DisplayFrame = true;
+            rasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables1.Handle = "2A";
+            rasterVariables1.Owner = null;
+            rasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument1.RasterVariables = rasterVariables1;
+            dxfDocument1.ThumbnailImage = null;
+            dxfDocument1.TotalEditTime = new System.DateTime(2023, 7, 12, 0, 0, 0, 0);
+            dxfDocument1.TotalEditTimeDays = 0;
+            dxfDocument1.UserTimer = new System.DateTime(2023, 7, 12, 0, 0, 0, 0);
+            dxfDocument1.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument1;
+            this.dxfReaderNETControl1.FileName = null;
+            this.dxfReaderNETControl1.HighlightGrabPoints = false;
+            this.dxfReaderNETControl1.HighlightGrabPointsColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(163)))), ((int)(((byte)(0)))));
+            this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
+            this.dxfReaderNETControl1.HighlightNotContinuous = false;
+            this.dxfReaderNETControl1.Location = new System.Drawing.Point(0, -2);
+            this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
+            this.dxfReaderNETControl1.ShowAxes = false;
+            this.dxfReaderNETControl1.ShowGrid = true;
+            this.dxfReaderNETControl1.ShowGridRuler = false;
+            this.dxfReaderNETControl1.Size = new System.Drawing.Size(600, 600);
+            this.dxfReaderNETControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 604);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load DXF file...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 604);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Zoom center";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 604);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Create image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(606, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(288, 604);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 25);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Create image B&&W";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(402, 604);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 25);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Create image grayscale";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(606, 604);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 25);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Save picture...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 632);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dxfReaderNETControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DXFReader.NET - Create images from DXF files";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DXFReaderNET.DXFReaderNETControl dxfReaderNETControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using DXFReaderNET;
+using DXFReaderNET.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DXFImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "DXF";
+            openFileDialog1.Filter = "DXF|*.dxf";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dxfReaderNETControl1.ReadDXF(openFileDialog1.FileName);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dxfReaderNETControl1.ZoomCenter();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DXFReaderNETControl _dxfControl;
+
+            _dxfControl = new DXFReaderNETControl();
+
+            _dxfControl.ReadDXF(dxfReaderNETControl1.FileName);
+            
+            _dxfControl.Width = 2000;
+            _dxfControl.Height = 2000;
+
+            _dxfControl.AntiAlias = false;
+            _dxfControl.ShowAxes = false;
+            _dxfControl.ShowGrid = false;
+            _dxfControl.BackColor = System.Drawing.Color.White;
+            _dxfControl.ForeColor = System.Drawing.Color.Black;
+
+
+            _dxfControl.ZoomCenter();
+            pictureBox1.Image = _dxfControl.Image;
+
+            _dxfControl.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+            DXFReaderNETControl _dxfControl;
+
+            _dxfControl = new DXFReaderNETControl();
+
+            _dxfControl.ReadDXF(dxfReaderNETControl1.FileName);
+
+            _dxfControl.Width = 2000;
+            _dxfControl.Height = 2000;
+
+            _dxfControl.AntiAlias = false;
+            _dxfControl.ShowAxes = false;
+            _dxfControl.ShowGrid = false;
+            _dxfControl.BackColor = System.Drawing.Color.White;
+            _dxfControl.ForeColor = System.Drawing.Color.Black;
+            foreach (EntityObject entity in _dxfControl.DXF.Entities)
+            {
+                entity.Color = AciColor.FromCadIndex(7);
+            }
+
+            foreach (DXFReaderNET.Blocks.Block _block in _dxfControl.DXF.Blocks.Items)
+            {
+                foreach (EntityObject ent in _block.Entities)
+                {
+                    ent.Color = AciColor.FromCadIndex(7);
+                }
+            }
+
+            _dxfControl.ZoomCenter();
+            pictureBox1.Image = _dxfControl.Image;
+
+            _dxfControl.Dispose();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = "jpg";
+            saveFileDialog1.Filter = "JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png|BMP (*.bmp)|*.bmp";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Title = "Export file";
+
+           
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        dxfReaderNETControl1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        dxfReaderNETControl1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case 3:
+                        dxfReaderNETControl1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DXFReaderNETControl _dxfControl;
+
+            _dxfControl = new DXFReaderNETControl();
+
+            _dxfControl.ReadDXF(dxfReaderNETControl1.FileName);
+
+            _dxfControl.Width = 2000;
+            _dxfControl.Height = 2000;
+
+            _dxfControl.AntiAlias = false;
+            _dxfControl.ShowAxes = false;
+            _dxfControl.ShowGrid = false;
+            _dxfControl.BackColor = System.Drawing.Color.White;
+            _dxfControl.ForeColor = System.Drawing.Color.Black;
+            foreach (EntityObject entity in _dxfControl.DXF.Entities)
+            {
+                Color color = entity.Color.ToColor();
+                if (entity.Color == AciColor.ByLayer)
+                {
+                    color = _dxfControl.DXF.Layers[entity.Layer.Name].Color.ToColor();
+                }
+                int grayScale = (int)((color.R * .3) + (color.G * .59) + (color.B * .11));
+                Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                entity.Color = new AciColor(newColor);
+            }
+
+            foreach (DXFReaderNET.Blocks.Block _block in _dxfControl.DXF.Blocks.Items)
+            {
+                foreach (EntityObject ent in _block.Entities)
+                {
+                    Color color = ent.Color.ToColor();
+                    if (ent.Color == AciColor.ByLayer)
+                    {
+                        color = _dxfControl.DXF.Layers[ent.Layer.Name].Color.ToColor();
+                    }
+                    int grayScale = (int)((color.R * .3) + (color.G * .59) + (color.B * .11));
+                    Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                    ent.Color = new AciColor(newColor);
+                }
+            }
+
+            _dxfControl.ZoomCenter();
+            pictureBox1.Image = _dxfControl.Image;
+
+            _dxfControl.Dispose();
+        }
+        private Color _GetGrayValue(Color color)
+        {
+            int grayScale = (int)((color.R * .3) + (color.G * .59) + (color.B * .11));
+            Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+            return newColor;
+        }
+    }
+}
